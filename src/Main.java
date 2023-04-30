2581f5262c89e7feb6317814aff62ef9a87a51c2
Jsilva221 <juasilva2021@inf.udec.cl>
2023-04-30 17:22:16 -0400

    Tarea 1 Programación 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Moneda m = null;
         Comprador c = null;
 
-        //-----Bebida que NO vende-----
+        System.out.println("Bebida que NO vende:");//-----Bebida que NO vende-----
         try{m = new Moneda500();
             c = new Comprador(m, 657, exp);
             System.out.println(c.queConsumiste() + ",vuelto " + c.cuantoVuelto());// null 500
@@ -15,19 +15,8 @@ public class Main {
             System.out.println(c.queConsumiste() + ",vuelto " + c.cuantoVuelto());// null 500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
-        try{m = new Moneda500();
-            c = new Comprador(m, 657, exp);
-            System.out.println(c.queConsumiste() + ",vuelto " + c.cuantoVuelto());// null 500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda500();
-            c = new Comprador(m, 657, exp);
-            System.out.println(c.queConsumiste() + ",vuelto " + c.cuantoVuelto());// null 500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-
-        //-----trata de comprar SIN Moneda-----
-        try{m = null;
+        System.out.println("trata de comprar SIN Moneda:");//-----trata de comprar SIN Moneda-----
+        try{//m = null
             c = new Comprador(null, Expendedor.SPRITE, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
@@ -37,102 +26,213 @@ public class Main {
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
+        System.out.println("con MENOS dinero que el precio:");//-----con MENOS dinero que el precio-----
+        try{m = new Moneda500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con dinero JUSTO para el precio:");//-----con dinero JUSTO para el precio-----
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con MAS dinero que el precio:");//-----con MAS dinero que el precio-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("Deposito VACIO:");//-----Deposito VACIO-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("trata de comprar SIN Moneda:");//-----trata de comprar SIN Moneda-----
+        try{//m = null
+            c = new Comprador(null, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
         try{//m=null
-            c = new Comprador(null, Expendedor.SPRITE, exp);
+            c = new Comprador(null, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con MENOS dinero que el precio:");//-----con MENOS dinero que el precio-----
+        try{m = new Moneda500();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda500();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con dinero JUSTO para el precio:");//-----con dinero JUSTO para el precio-----
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con MAS dinero que el precio:");//-----con MAS dinero que el precio-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("Deposito VACIO:");//-----Deposito VACIO-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+        //////////////////////////////////////////////////////////
+        System.out.println("trata de comprar SIN Moneda:");//-----trata de comprar SIN Moneda-----
+        try{//m = null
+            c = new Comprador(null, Expendedor.SNKR, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
         try{//m=null
-            c = new Comprador(null, Expendedor.SPRITE, exp);
+            c = new Comprador(null, Expendedor.SNKR, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
-        //-----con dinero JUSTO para el precio-----
-        try{m = new Moneda1000();
-            c = new Comprador(m, Expendedor.COCA, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda1000();
-            c = new Comprador(m, Expendedor.COCA, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda1000();
-            c = new Comprador(m, Expendedor.COCA, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda1000();
-            c = new Comprador(m, Expendedor.COCA, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        //-----con MENOS dinero que el precio-----
+        System.out.println("con MENOS dinero que el precio:");//-----con MENOS dinero que el precio-----
         try{m = new Moneda500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-        try{m = new Moneda500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+            c = new Comprador(m, Expendedor.SNKR, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
         try{m = new Moneda500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con dinero JUSTO para el precio:");//-----con dinero JUSTO para el precio-----
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con MAS dinero que el precio:");//-----con MAS dinero que el precio-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("Deposito VACIO:");//-----Deposito VACIO-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SNKR, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("trata de comprar SIN Moneda:");//-----trata de comprar SIN Moneda-----
+        try{//m = null
+            c = new Comprador(null, Expendedor.SPR8, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        try{//m=null
+            c = new Comprador(null, Expendedor.SPR8, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// sprite 500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("con MENOS dinero que el precio:");//-----con MENOS dinero que el precio-----
+        try{m = new Moneda500();
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
         try{m = new Moneda500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
-        //-----con MAS dinero que el precio-----
-        try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+        System.out.println("con dinero JUSTO para el precio:");//-----con dinero JUSTO para el precio-----
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
-        try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+        try{m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
+        System.out.println("con MAS dinero que el precio:");//-----con MAS dinero que el precio-----
         try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-
-        //-----Deposito VACIO-----
-        try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
         try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+            c = new Comprador(m, Expendedor.SPR8, exp);
+            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
+        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
+
+        System.out.println("Deposito VACIO:");//-----Deposito VACIO-----
+        try{m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
         try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
-            System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
-        }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
-
-        try{m = new Moneda1500();
-            c = new Comprador(m, Expendedor.SPRITE, exp);
+            c = new Comprador(m, Expendedor.SPR8, exp);
             System.out.println(c.queConsumiste() + ",vuelto: " + c.cuantoVuelto());// null 1500
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e ) {System.out.println(e);}
 
